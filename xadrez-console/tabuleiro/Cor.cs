634d258a3c1ebddada053e4ad2df57553987f1ff
Luciano Chaves <lucianochr@hotmail.com>
2018-05-22 21:03:27 -0300

    Comecando a implementas o tabuleiro

---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,14 @@
+﻿namespace xadrez_console.tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+
+    }
+}
